7a380fa25b90a0c671ef6526712adade9240edc0
Jonathas Santos <jonathas.santos@meliuz.com.br>
2021-02-12 16:57:52 -0400

    feat: Refatora o componente de recomendacao

--- src/App.js
+++ src/App.js
@@ -1,17 +1,63 @@
+import { Component } from 'react';
 import './App.css';
 
-import Recommendation from './components/recommendation/Recommendation'
+import Guidelines from './components/guidelines/Guidelines'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Recomendações de interface para autistas</h1>
-      <Recommendation></Recommendation>
-      <Recommendation></Recommendation>
-      <Recommendation></Recommendation>
+class App extends Component {
 
-    </div>
-  );
+  state = {
+    guidelines: [
+      {
+        id: 1,
+        category: 'Interface',
+        description: 'Colocar mais cores',
+        createdAt: new Date(2021, 2, 1),
+        authorEmail: 'jonathas.silvasantos@gmail.com',
+        likes: '3',
+        unlikes: '0'
+      },
+      {
+        id: 2,
+        category: 'Interface',
+        description: 'Colocar mais cores',
+        createdAt: new Date(2021, 2, 1),
+        authorEmail: 'jonathas.silvasantos@gmail.com',
+        likes: '3',
+        unlikes: '0'
+      },
+      {
+        id: 3,
+        category: 'Interface',
+        description: 'Colocar mais cores',
+        createdAt: new Date(2021, 2, 1),
+        authorEmail: 'jonathas.silvasantos@gmail.com',
+        likes: '3',
+        unlikes: '0'
+      }
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Recomendações de interface para autistas</h1>
+
+        {this.state.guidelines.map(guideline => (
+          <Guidelines
+            key={guideline.id}
+            category={guideline.category}
+            description={guideline.description}
+            createdAt={guideline.createdAt}
+            authorEmail={guideline.authorEmail}
+            likes={guideline.likes}
+            unlikes={guideline.unlikes}
+          >
+          </Guidelines>
+        ))}
+      </div>
+    );
+  }
+
 }
 
 export default App;
